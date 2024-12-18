0d3e4b145cde5dab098400d533249a1efd3ba6ab
Alexandre Saussier <asaussier99@gmail.com>
2024-12-18 19:47:57 +0100

    fixes

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -3,7 +3,6 @@ import { connectDB } from '@/lib/mongodb'
 import { Project } from '@/models/Project'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
-import mongoose from 'mongoose'
 
 
 export async function POST(request: Request) {
