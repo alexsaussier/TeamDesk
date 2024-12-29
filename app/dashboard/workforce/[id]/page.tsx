0195affd6fbe174a82119569da53fdca5e396d2e
Alexandre Saussier <asaussier99@gmail.com>
2024-12-29 16:35:56 +0100

    fixes

--- app/dashboard/workforce/[id]/page.tsx
+++ app/dashboard/workforce/[id]/page.tsx
@@ -1,9 +1,11 @@
 import ConsultantDetails from '@/components/ConsultantDetails'
 
 export default async function ConsultantPage({ params: { id } }: { params: { id: string } }) {
+  const consultantId = await id
+
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <ConsultantDetails consultantId={id} />
+      <ConsultantDetails consultantId={consultantId} />
     </div>
   )
 } 
