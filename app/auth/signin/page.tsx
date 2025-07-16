70f38014a7efbad8c7de6c91ebb8d4028bc450a4
alexsaussier <asaussier99@gmail.com>
2025-07-16 08:33:08 +0100

    fixes

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -41,7 +41,7 @@ export default function SignInPage() {
       } else if (result?.ok) {
         router.push('/dashboard')
       }
-    } catch (_) {
+    } catch (_error) {
       setError('An error occurred. Please try again.')
     }
   }
